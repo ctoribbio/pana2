d67b3171b3989b2ff8e4b7a4e7022e98af73c9de
ctoribbio <cesartoribiocastro@gmail.com>
2022-05-31 02:24:44 +0200

    final

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -124,7 +124,7 @@ sentiment_very_dissatisfied
   <footer class="bg-light py-5">
     <div class="container px-4 px-lg-5">
       <div class="small text-center text-muted">
-        Copyright &copy; 2022 - Company Name
+        Copyright &copy; 2022 - PANA.- César Toribio DAW
       </div>
     </div>
   </footer>
